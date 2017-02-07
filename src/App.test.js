8e04adf939eb258993e3f17cac507d33c849c294
xdeveloper <alexander.abakumov@gmail.com>
2017-02-07 16:00:49 +0200

    Detect swap direction

--- src/App.test.js
+++ src/App.test.js
@@ -8,12 +8,26 @@ it('renders without crashing', () => {
     ReactDOM.render(<App />, div);
 });
 
+function inArray(needle, haystack) {
+    var count = haystack.length;
+    for (var i = 0; i < count; i++) {
+        if (haystack[i] === needle) {
+            return true;
+        }
+    }
+    return false;
+}
+
 
 test('generates ok', () => {
     let field = new Core().generate(5);
 
-    expect(field).toEqual([[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]])
-
+    // Contain only 1, 2, 3, 4 balls (so - 5 is illegal ball)
+    expect(inArray(5, field[0])).toBeFalsy();
+    expect(inArray(5, field[1])).toBeFalsy();
+    expect(inArray(5, field[2])).toBeFalsy();
+    expect(inArray(5, field[3])).toBeFalsy();
+    expect(inArray(5, field[4])).toBeFalsy();
 });
 
 test('generates fail', () => {
@@ -21,4 +35,15 @@ test('generates fail', () => {
         new Core().generate(2);
     }).toThrow(new Error("Size of field must be 5 x 5 minimum"));
 
+});
+
+test('detect swap direction', () => {
+    let core = new Core();
+
+    expect(core.detectSwapDirection([0, 0], [1, 0])).toEqual({direction: 'vertical'});
+    expect(core.detectSwapDirection([0, 0], [0, 1])).toEqual({direction: 'horizontal'});
+    expect(core.detectSwapDirection([0, 0], [0, 0])).toEqual({direction: 'illegal'}); // the same
+    expect(core.detectSwapDirection([0, 0], [1, 1])).toEqual({direction: 'illegal'}); // diagonal
+    expect(core.detectSwapDirection([0, 0], [0, 2])).toEqual({direction: 'illegal'}); // too far
+    expect(core.detectSwapDirection([0, 0], [2, 2])).toEqual({direction: 'illegal'}); // diagonal too far
 });
--- src/core.js
+++ src/core.js
@@ -16,7 +16,7 @@ class Core {
             let row = new Array(r);
 
             for (let c = 0; c < n; c++) {
-                row[c] = 0;
+                row[c] = this.generateBall();
             }
 
             whole[r] = row;
@@ -24,6 +24,39 @@ class Core {
 
         return whole;
     }
+
+    generateBall() {
+        return Math.floor(Math.random() * (4 - 1 + 1)) + 1;
+    }
+
+    /**
+     * Vertical, Horizontal, Illegal
+     */
+    detectSwapDirection(firstBallCoords, secondBallCoords) {
+
+        function neighbours(pos1, pos2) {
+            return (Math.abs(pos1 - pos2) == 1);
+        }
+
+        let [row1, col1] = firstBallCoords;
+        let [row2, col2] = secondBallCoords;
+
+        let direction;
+        if (col1 === col2 && neighbours(row1, row2)) {
+            direction = VERTICAL;
+        } else if (row1 === row2 && neighbours(col1, col2)) {
+            direction = HORIZONTAL;
+        } else {
+            direction = ILLEGAL;
+        }
+
+        return {direction: direction}
+    }
+
 }
+
+const VERTICAL = 'vertical';
+const HORIZONTAL = 'horizontal';
+const ILLEGAL = 'illegal';
 
 export default Core;
